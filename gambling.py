9636e5ab23149f9cba5f1edbbd031b0ad54bdbbd
shadowninja71 <viktor.hvidpersson@elev.ga.ntig.se>
2024-09-05 09:37:24 +0200

    tärnings spel

---
+++ gambling.py
@@ -0,0 +1,13 @@
+from random import randint
+rule = input("odd or even")
+computer = randint(1, 6)
+player = randint(1, 6)
+
+if rule == "udda"
+  computer_result = computer % 2
+  player_result = player % 2
+  print(f"datorn rullade {computer_resulte} och spelaren {player_result}, spelareglen var {rule}")
+else:
+  computer_result = computer % 2
+  player_result = player % 2
+  print(f"")
