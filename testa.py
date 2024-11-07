e993142917bd275316af1332088bcf2c692f3671
shadowninja71 <viktor.hvidpersson@elev.ga.ntig.se>
2024-11-07 08:50:12 +0100

    gjrode statmenten bettre

--- testa.py
+++ testa.py
@@ -2,7 +2,7 @@ tal1 = 18
 tal2 = int(input("skriv in ett tal:"))
 
 if tal2 > tal1 :
-    print(f"ditt tal var större, dit tal:{tal2}")
+    print("ditt tal var större, dit tal:",tal2)
 
 else:
-    print(f"datorns tal är störe,datorns tal:{tal1}")
+    print("datorns tal är störe,datorns tal:",tal1)
