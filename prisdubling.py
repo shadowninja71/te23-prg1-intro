b3f2530d1701327738158c102309e0bc1eb36379
shadowninja71 <viktor.hvidpersson@elev.ga.ntig.se>
2025-01-30 09:46:30 +0100

    klar

---
+++ prisdubling.py
@@ -0,0 +1,7 @@
+skänat = 0.1
+dag = 1
+print("antal dagar:", dag , "antal skänat:", skänat)
+while skänat <= 1000000:
+    dag = dag + 1
+    skänat = skänat * 2
+    print("antal dagar:", dag , "antal skänat:", skänat)
